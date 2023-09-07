f88763708e1ffe374d9a1da6c5aa3c657625d63a
Parag Chauhan <paragchauhan33@gmail.com>
2023-09-07 19:03:31 +0530

    First Commit

---
+++ src/components/Home/Home.js
@@ -0,0 +1,58 @@
+import React, { useEffect } from 'react';
+import { Link } from 'react-router-dom';
+import hill1 from "./Img/hill1.png";
+import hill2 from "./Img/hill2.png";
+import hill3 from "./Img/hill3.png";
+import hill4 from "./Img/hill4.png";
+import hill5 from "./Img/hill5.png";
+import tree from "./Img/tree.png";
+import leaf from "./Img/leaf.png";
+import plant from "./Img/plant.png";
+import "./Home.css";
+import Banner from '../Banner/Banner';
+
+
+const ScrollingEffects = () => {
+  useEffect(() => {
+    const handleScroll = () => {
+      let value = window.scrollY;
+
+      // Adjust these values as needed for your specific layout
+      document.getElementById('text').style.marginTop = value * 2.5 + "px";
+      document.getElementById('leaf').style.top = value * -1.5 + "px";
+      document.getElementById('leaf').style.left = value * 1.5 + "px";
+      document.getElementById('hill5').style.left = value * 1.5 + 'px';
+      document.getElementById('hill4').style.left = value * -1.5 + 'px';
+      document.getElementById('hill1').style.top = value * 1 + 'px';
+    };
+
+    window.addEventListener("scroll", handleScroll);
+
+    // Cleanup the event listener when the component unmounts
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+
+  return (
+    <div className="body">
+
+    
+      <div className="container">
+
+        <img alt="tree" src={hill1} id="hill1" />
+        <img alt="tree" src={hill2} id="hill2" />
+        <img alt="tree" src={hill3} id="hill3" />
+        <img alt="tree" src={hill4} id="hill4" />
+        <img alt="tree" src={hill5} id="hill5" />
+        <img alt="tree" src={tree} id="tree" />
+        <h2 id="text">Density Animated Website Project</h2>
+        <img alt="tree" src={leaf} id="leaf" />
+        <img alt="tree" src={plant} id="plant" />
+      </div>
+
+    </div>
+  );
+};
+
+export default ScrollingEffects;
